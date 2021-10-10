4b4474e4ef67532a9539c12d3e7c19617a8fad35
Fhenriqueoliveira <fhenrique.sistemas@gmail.com>
2021-10-10 11:09:59 -0300

    imports prisma module
    Signed-off-by: Fhenriqueoliveira <fhenrique.sistemas@gmail.com>

--- src/genero/genero.module.ts
+++ src/genero/genero.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GenerosService } from './genero.service';
 import { GenerosController } from './genero.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [GenerosService],
   controllers: [GenerosController],
 })
--- src/participante/participante.module.ts
+++ src/participante/participante.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ParticipantesService } from './participante.service';
 import { ParticipantesController } from './participante.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [ParticipantesService],
   controllers: [ParticipantesController],
 })
